5405ce092a7f0787821456e1ff660661a6c54273
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-09 23:45:33 +0300

    Update function.js

--- faq/function.js
+++ faq/function.js
@@ -4,7 +4,6 @@ const items = document.querySelectorAll(".accordion a");
 function toggleAccordion(){
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
-  siblings('li.a').toggle('deactivate');
 
 }
  
