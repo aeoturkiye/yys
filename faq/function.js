5883e11040510f12cfdb950a95339fda46f1ecca
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-09 23:18:43 +0300

    Update function.js

--- faq/function.js
+++ faq/function.js
@@ -4,7 +4,7 @@ const items = document.querySelectorAll(".accordion a");
 function toggleAccordion(){
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
-  this.siblings('li.a').slideUp();
+  siblings('li.a').slideUp();
 
 }
  
