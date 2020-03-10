c3d610a5ef5953bebd50cf6ae8ed76d79f16590a
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 12:50:10 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -8,8 +8,8 @@ function toggleAccordion(){
   for (i = 0; i < acc.length; i++) {
   var active = document.querySelector(".accordion a");
     if (active && active != this) {
-      //active.classList.remove("active");
-      active.nextElementSibling.classList.toggle('passive');
+      active.classList.remove("active");
+      active.nextElementSibling.classList.remove('active');
     }
     }  
   
