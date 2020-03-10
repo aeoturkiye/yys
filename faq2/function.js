1e0684040b10d5cae1966f37270115855b625b14
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 12:39:03 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -3,7 +3,8 @@ const items = document.querySelectorAll(".accordion a");
 var acc = document.getElementsByClassName("content");
 
 function toggleAccordion(){
-  alert(accx[0].style.display);
+  //var acc = document.getElementsByClassName("content");
+  alert(acc[0].style.display);
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
   //this.nextElementSibling.classList.toggle('passive');
