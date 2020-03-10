a363e91ee713bfda98ee8cdf6a5cce589cf5f43a
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 12:51:57 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -3,16 +3,7 @@ const items = document.querySelectorAll(".accordion a");
 function toggleAccordion(){
   var acc = document.getElementsByClassName("content");
   alert(acc.length);
-  
-  var i;
-  for (i = 0; i < acc.length; i++) {
-  var active = document.querySelector(".accordion a");
-    if (active && active != this) {
-      active.classList.remove("active");
-      active.nextElementSibling.classList.remove('active');
-    }
-    }  
-  
+    
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
 }
