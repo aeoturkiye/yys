ac8453d87e83f2be0f31180050324af4e9a7df03
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 12:18:47 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -1,16 +1,10 @@
+// Code By Webdevtrick ( https://webdevtrick.com )
 const items = document.querySelectorAll(".accordion a");
-var acc = document.getElementsByClassName("accordion-item");
-alert(acc);
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    var active = document.querySelector(".accordion a");
-    if (active && active != this) {
-      active.classList.remove("active");
-      active.nextElementSibling.classList.remove("show");
-    }
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("show");
-  }
+var acc = document.getElementsByClassName("accordion-item"); 
+function toggleAccordion(){
+  alert(acc);
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
 }
+ 
+items.forEach(item => item.addEventListener('click', toggleAccordion));
