801309171985f478b1eb8cf7c03760930a77d6c6
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 11:09:10 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -6,7 +6,7 @@ var speed = "500";
 $(document).ready(function(){
 
 // Question handler
-  $('accordion.a').on(action, function(){
+  $('accordian.a').on(action, function(){
 
     // gets next element
     // opens .a of selected question
