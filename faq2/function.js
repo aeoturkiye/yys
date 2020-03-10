e3e54637139d980ec6ac7f7a7e792c175e924868
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 12:00:16 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -4,7 +4,7 @@ const items = document.querySelectorAll(".accordion a");
 function toggleAccordion(){
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
-  not(this).siblings.classList.slideUp();
+  not(this).siblings.classList.toggle('hide');
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
