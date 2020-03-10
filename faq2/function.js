9ae46c1a93803355a7870a3d0d99deb5d2fd310b
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 12:41:18 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -1,9 +1,9 @@
 // Code By Webdevtrick ( https://webdevtrick.com )
 const items = document.querySelectorAll(".accordion a");
-var acc = document.getElementsByClassName("content");
+//var acc = document.getElementsByClassName("content");
 
 function toggleAccordion(){
-  //var acc = document.getElementsByClassName("content");
+  var acc = document.getElementsByClassName("content");
   alert(acc.length);
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
