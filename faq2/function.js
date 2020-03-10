cbab36605f557286694b3e0c43c18ce157914551
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 15:01:13 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -5,19 +5,19 @@ function toggleAccordion(){
 this.classList.toggle('active');
 this.nextElementSibling.classList.toggle('active'); 
   
-if this.classList.hasClass("active"){
-  alert("active");
-} else {
-alert("not");
-}
+//if this.classList.hasClass("active"){
+//  alert("active");
+//} else {
+//alert("not");
+//}
   
-  
+}  
 //var acc = document.getElementsByClassName("content");
 //var i;
 //  for (i = 0; i < acc.length; i++) {  
 //  items[i].classList.toggle('active');
 //  items[i].nextElementSibling.classList.toggle('active');
-  }
+//  }
   
 //  this.classList.toggle('active');
 //  this.nextElementSibling.classList.toggle('active'); 
