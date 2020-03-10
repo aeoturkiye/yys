f216e303fe64a08f45e81c656164eb347cb2b667
aeoturkiye <61291920+aeoturkiye@users.noreply.github.com>
2020-03-10 14:59:51 +0300

    Update function.js

--- faq2/function.js
+++ faq2/function.js
@@ -1,22 +1,27 @@
 const items = document.querySelectorAll(".accordion a");
 
 function toggleAccordion(){
-  
-var acc = document.getElementsByClassName("content");
-var i;
-  for (i = 0; i < acc.length; i++) {  
-  items[i].classList.toggle('active');
-  items[i].nextElementSibling.classList.toggle('active');
-  }
 
-  for (i = 0; i < acc.length; i++) {  
-  items[i].classList.toggle('active');
-  items[i].nextElementSibling.classList.toggle('active');
+this.classList.toggle('active');
+this.nextElementSibling.classList.toggle('active'); 
+  
+if this.classList.hasClass("active"){
+  alert("active");
+} else {
+alert("not");
+}
+  
+  
+//var acc = document.getElementsByClassName("content");
+//var i;
+//  for (i = 0; i < acc.length; i++) {  
+//  items[i].classList.toggle('active');
+//  items[i].nextElementSibling.classList.toggle('active');
   }
   
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active'); 
+//  this.classList.toggle('active');
+//  this.nextElementSibling.classList.toggle('active'); 
   
-} 
+//} 
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
